4e4fe907f196c34737ab248fdb28b4d0bc8b2ec0
SanderStudio <venommax8@gmail.com>
2024-04-09 17:27:08 +0300

    Added Player class

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,26 +1,28 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class Player : Person
-//{
-//    public int ExpPoint {  get; private set; }
+public class Player : Person
+{
+    public int ExpPoint { get; private set; } = 50;
 
-//    [SerializeField] private int _expPoints = 50;
+    [SerializeField] private int _expPoints = 50;
 
-//    public void Start()
-//    {
-//        Debug.Log("Start Player class");
-//        Debug.Log(Health);
-//        Debug.Log(PersonName);
+    public void Start()
+    {
+        Debug.Log("Start Player class");
+        Debug.Log(Health);
+        Debug.Log(PersonName);
+        Debug.Log(ExpPoint);
 
-//        Health = 60;
-//        PersonName = "Builder";
+        Health = 60;
 
+        Debug.Log(Health);
 
-//        ShowStat();
-//        Debug.Log("End Player class");
-//    }
+        //ShowStat();
+        Debug.Log("End Player class");
+    }
+}
 
 //    public override void ShowStat()
 //    {
